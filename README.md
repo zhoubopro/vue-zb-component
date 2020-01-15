2a2d41ea201cfcc273f2e363e92ec1418b3edef3
周博 <zhoubo@reignwood.com>
2020-01-15 20:52:14 +0800

    add icon iconPosition

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ parcel
 【报错】运行 parcel 时，出现 No entries found 报错怎么办？
 把./node_modules/.bin/parcel
 改成./node_modules/.bin/parcel index.html --no-cache 即可
+ 
+简写 npx parcel --no-cache index.html
+
+[can i use](www.caniuse.com)
--- src/component/button/button.vue
+++ src/component/button/button.vue
@@ -1,13 +1,29 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="svg-icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 <script>
   export default {
-
+    props: {
+      icon: {
+        type: String,
+        default: ''
+      },
+      iconPosition: {
+        type: String,
+        default: 'left'
+      }
+    }
   }
 </script>
 <style lang="scss">
-  .g-button{
+  .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -15,14 +31,34 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover{
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    &:hover {
       border-color: var(--border-color-hover);
     }
-    &:active{
+    &:active {
       background: var(--button-active-bg);
     }
-    &:focus{
-      outline: none ;
+    &:focus {
+      outline: none;
+    }
+    > .svg-icon {
+      order: 1;
+      margin: .1em .2em 0 0;
+    }
+    > .content {
+      order: 2;
+    }
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+      > .svg-icon {
+        order: 2;
+        margin: .1em 0 0 .2em;
+      }
     }
   }
 </style>
