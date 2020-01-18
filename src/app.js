d9f0fe8ed665ef7f8461dd7cbc6160ebca63a29a
周博 <zhoubo@reignwood.com>
2020-01-18 13:44:25 +0800

    create button-group

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import ButtonGroup from './component/button-group/index.js';
 import Button from './component/button/index.js';
 import Icon from './component/icon/index.js';
 
+Vue.component('g-button-group', ButtonGroup);
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 
---
+++ src/component/button-group/button-group.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    mounted () {
+      for (let node of this.$el.children) {
+        let nodeName = node.nodeName.toLocaleLowerCase()
+        if (nodeName !== 'button') {
+          console.warn(`g-button-group 子元素都应该是button,但是你写的是${nodeName}`)
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .g-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .g-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
