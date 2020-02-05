3b046c07befbf3d3bccdf82fadbcae050e85704b
周博 <zhoubo@reignwood.com>
2020-02-05 20:01:57 +0800

    使用vue-cli启动

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <g-button icon="set">
+      按钮
+    </g-button>
+  </div>
+</template>
+
+<script>
+  import Button from './component/button'
+
+  export default {
+    name: 'app',
+    components: {
+      'g-button': Button
+    }
+  }
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  img {
+    max-width: 100%;
+  }
+  html {
+    --font-size: 14px;
+  }
+  :root {
+    --button-height: 32px;
+    --font-size: 14px;
+    --button-bg: white;
+    --button-active-bg: #eee;
+    --border-radius: 4px;
+    --color: #999;
+    --border-color: #999;
+    --border-color-hover: #666;
+  }
+  body {
+    font-size: var(--font-size);
+  }
+</style>
